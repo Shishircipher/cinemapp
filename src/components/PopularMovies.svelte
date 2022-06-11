d2f6b541833ab2e2a39a1a63cfc77af63c86038f
shishir chaurasiya <shishirchaurasiya17@gmail.com>
2022-06-12 01:09:11 +0530

    first commit

---
+++ src/components/PopularMovies.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let popular;
+    console.log(popular)
+</script>
+<h3>
+    Popular Movies
+</h3>
+<div class="popular-movies">
+    {#each popular as movie}
+    <h1> {movie.title} </h1>
+    {/each}
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+    export async function load({fetch}){
+        const res = await fetch (
+            `https://api.themoviedb.org/3/movie/popular?api_key=language=en-US&page=1`
+            
+        );
+        const data = await res.json();
+        if(res.ok){
+            return {
+                props: {popular: data.results}
+
+            };
+        
+    }
+}
+
+</script>
+
+
+<script>
+    import PopularMovies from '../components/PopularMovies.svelte';
+    export let popular;
+
+</script>
+
+<section>
+    <PopularMovies {popular}/>
+</section>
